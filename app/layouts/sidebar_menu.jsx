7799f69f51a11092d0e2bcf9c91f9947610a22e5
Maciej Gawel <m.gawel@hotmail.com>
2016-12-04 14:11:38 +0100

    Add repositories to sidebar

--- app/layouts/sidebar_menu.jsx
+++ app/layouts/sidebar_menu.jsx
@@ -1,13 +1,22 @@
-import React              from 'react';
-import { connect }        from 'react-redux';
-import { Sidebar, Menu, Image, Icon }  from 'semantic-ui-react';
+import React                            from 'react';
+import { connect }                      from 'react-redux';
+import { push }                         from 'react-router-redux';
+import { Sidebar, Menu, Image, Icon }   from 'semantic-ui-react';
 
-import SessionActions     from '../actions/sessions';
+import SessionActions       from '../actions/sessions';
+import RepositoriesActions  from '../actions/repositories';
 
 class SidebarMenu extends React.Component {
   constructor() {
     super();
     this.handleSignOutClick = this.handleSignOutClick.bind(this);
+    this.handleRepositoryClick = this.handleRepositoryClick.bind(this);
+    this.renderRepositories = this.renderRepositories.bind(this);
+    this.renderIntegratedRepositories = this.renderIntegratedRepositories.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.dispatch(RepositoriesActions.fetchRepositories());
   }
 
   handleSignOutClick(e) {
@@ -16,15 +25,43 @@ class SidebarMenu extends React.Component {
     this.props.dispatch(SessionActions.signOut());
   }
 
+  handleRepositoryClick(repositoryId) {
+    this.props.dispatch(push(`/repositories/${repositoryId}`));
+  }
+
+  renderRepositories(repositories) {
+    return repositories.map(repository =>
+      <Menu.Item key={repository.id} onClick={() => this.handleRepositoryClick(repository.id)}>
+        {repository.name}
+      </Menu.Item>,
+    );
+  }
+
+  renderIntegratedRepositories() {
+    const { integratedRepositories } = this.props;
+
+    if (integratedRepositories.length === 0) return false;
+
+    return (
+      <Menu.Item name="repositories">
+        Integrated repositories
+        <Menu.Menu>
+          {this.renderRepositories(integratedRepositories)}
+        </Menu.Menu>
+      </Menu.Item>
+    );
+  }
+
   render() {
     return (
-      <Sidebar as={Menu} animation="push" width="thin" visible icon="labeled" vertical inverted>
-        <Menu.Item name="logo">
-          <Image src="img/logo.png" size="small" centered />
+      <Sidebar as={Menu} animation="push" width="wide" visible icon="labeled" vertical inverted>
+        <Menu.Item name="logo" onClick={() => this.props.dispatch(push('/'))}>
+          <Image src="/img/logo.png" size="small" centered />
         </Menu.Item>
         <Menu.Item name="user">
           {this.props.currentUser.name}
         </Menu.Item>
+        {this.renderIntegratedRepositories()}
         <Menu.Item onClick={this.handleSignOutClick} name="signOut">
           <Icon name="sign out" />
           SignOut
@@ -37,10 +74,12 @@ class SidebarMenu extends React.Component {
 SidebarMenu.propTypes = {
   dispatch: React.PropTypes.func.isRequired,
   currentUser: React.PropTypes.object.isRequired,
+  integratedRepositories: React.PropTypes.array.isRequired,
 };
 
 const mapStateToProps = state => ({
   currentUser: state.session.currentUser,
+  integratedRepositories: state.repositories.integratedRepositories,
 });
 
 export default connect(mapStateToProps)(SidebarMenu);
