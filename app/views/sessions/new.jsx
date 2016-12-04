ef277a08c1a5b24d2454e799520f97063dff2a13
Maciej Gawel <m.gawel@hotmail.com>
2016-12-04 18:29:06 +0100

    Homepage adjustments

--- app/views/sessions/new.jsx
+++ app/views/sessions/new.jsx
@@ -30,12 +30,23 @@ class SessionsNew extends React.Component {
             <h2 className="ui teal image header">
               <Image src="img/logo.png" size="medium" centered />
             </h2>
+            <h1>Pug Continuous Integration</h1>
+            <div className="sub header">
+              Why did the pug cross the road?
+              <br />
+              To get to the Barking Lot!
+            </div>
             <div className="ui stacked">
               <Button basic size="big" onClick={this.handleLoginWithGithubClick}>
                 <Icon name="github" /> Login with github
               </Button>
             </div>
           </div>
+        </div>
+        <div className="footer">
+          IOSR 2016
+          <br />
+          By Maciej Gaweł & Bartosz Żurkowski
         </div>
       </div>
     );
